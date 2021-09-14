a6d22d1142be545a29ed8258fb91c614029483c6
avinash <avinashDemo@outlook.com>
2021-09-14 21:01:49 +0530

    demo2

--- instance.tf
+++ instance.tf
@@ -1,11 +1,9 @@
 provider "aws" {
-  access_key = "AKIATPCJ7NLZY6MW56WZ"
-  secret_key = "+YaTbTE+IQFnY1ZNmSLq9Y+bvf/QMgKMtqwn2vgw"
-  region     = "ap-south-1"
+  region     = var.region
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0a23ccb2cdd9286bb"
+resource "aws_instance" "myserver" {
+  ami           = var.ami
   instance_type = "t2.micro"
   tags = {
     Name = "myserver"
